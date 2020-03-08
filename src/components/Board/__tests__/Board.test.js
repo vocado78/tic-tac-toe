1442e12f2f78e790ccdf8ce03eac37bda2f48e2f
vocado78 <hedeliningrid@gmail.com>
2020-03-08 14:55:52 -0700

    adds Board tests

---
+++ src/components/Board/__tests__/Board.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Board from '../Board';
+
+describe('Board', () => {
+  it('renders 3 rows with a total of 9 cells with default props', () => {
+    const board = Array(9).fill(null);
+    const totalNumOfRows = 3;
+    const totalNumOfCells = 9;
+
+    const { container } = render(<Board board={board} handleSquareClick={() => {}} />);
+    expect(container.querySelectorAll('tr').length).toEqual(totalNumOfRows);
+    expect(container.querySelectorAll('td').length).toEqual(totalNumOfCells);
+  });
+});
