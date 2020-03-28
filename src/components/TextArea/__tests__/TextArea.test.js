6ba378542c95cb319daf732c30456c0b0d3f0bf0
vocado78 <hedeliningrid@gmail.com>
2020-03-28 13:18:51 -0700

    adds test cases for text area comp

---
+++ src/components/TextArea/__tests__/TextArea.test.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import TextArea from '../TextArea';
+import { QUESTIONS, STATUS } from '../../../constants';
+
+
+const board = Array(9).fill(null);
+const renderTextArea = (singlePlayer = null, nextPlayer = '') => ({
+  ...render(
+    <TextArea
+      board={board}
+      setGameType={() => {}}
+      toggleNextPlayer={() => {}}
+      singlePlayer={singlePlayer}
+      nextPlayer={nextPlayer}
+    />
+  )
+});
+
+describe('TextArea', () => {
+  it('renders game type preference only with default props', () => {
+    const { queryByText } = renderTextArea();
+
+    expect(queryByText(QUESTIONS.GAME_TYPE)).toBeInTheDocument();
+    expect(queryByText(QUESTIONS.CHAR_TYPE_SINGLE)).not.toBeInTheDocument();
+    expect(queryByText(QUESTIONS.CHAR_TYPE_PAIR)).not.toBeInTheDocument();
+  });
+
+  it('renders char type preference only if singlePlayer is true or false', () => {
+    const singlePlayer = true;
+    const { queryByText } = renderTextArea(singlePlayer);
+
+    expect(queryByText(QUESTIONS.CHAR_TYPE_SINGLE)).toBeInTheDocument();
+    expect(queryByText(QUESTIONS.GAME_TYPE)).not.toBeInTheDocument();
+  });
+
+  it('renders a status message but not any preference if nextPlayer is truthy', () => {
+    const nextPlayer = 'X';
+    const { queryByText } = renderTextArea(null, nextPlayer);
+
+    expect(queryByText(`${STATUS.NEXT}${nextPlayer}`)).toBeInTheDocument();
+    expect(queryByText(QUESTIONS.GAME_TYPE)).not.toBeInTheDocument();
+    expect(queryByText(QUESTIONS.CHAR_TYPE_SINGLE)).not.toBeInTheDocument();
+    expect(queryByText(QUESTIONS.CHAR_TYPE_PAIR)).not.toBeInTheDocument();
+  });
+});
